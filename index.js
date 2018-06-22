f238f6e99c3ebb0785c0e3f88afda415258ebbe4
Rishabh Shukla <rishbabhshukla25@hotmail.com>
2018-06-23 02:52:15 +0530

    Added user Add and user Delete routes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const redis = require('redis');
 
 // create redis client
 const client = redis.createClient();
-client.on('connect', function () {
+client.on('connect', () => {
   console.log('Connected to Redis')
 });
 
@@ -33,11 +33,38 @@ app.get('/', (req, res, next) => {
   res.render('searchusers');
 });
 
+// add user page
+app.get('/user/add', (req, res, next) => {
+  res.render('addUser');
+});
+
+// add user processing
+app.post('/user/add', (req, res, next) => {
+  let id = req.body.id;
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let email = req.body.email;
+  let phone = req.body.phone;
+
+  client.hmset(id, [
+      'first_name', first_name,
+      'last_name', last_name,
+      'email', email,
+      'phone', phone
+  ], (err, reply) => {
+    if(err){
+      console.log(err);
+    }
+    console.log(reply);
+    res.redirect('/');
+  });
+});
+
 // search processing
 app.post('/user/search', (req, res, next) => {
   let id = req.body.id;
 
-  client.hgetall(id, function (err, obj) {
+  client.hgetall(id, (err, obj) => {
     if(!obj){
       res.render('searchusers', {
         error: 'User does not exist'
@@ -51,6 +78,12 @@ app.post('/user/search', (req, res, next) => {
   });
 });
 
-app.listen(port, function () {
+// delete user
+app.delete('/user/delete/:id', (req, res, next) => {
+  client.del(req.params.id);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
   console.log('Server started on port: ' + port);
 });
